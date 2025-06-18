9d9f69db8ed04fb676323c48782d659e456b1abc
notalihassan <alihassan92199@gmail.com>
2025-06-18 09:31:50 +0500

    add

---
+++ Learning_ChaiAurCode/01_basics/02_datTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser. it will work on the browser
+// we can also run this in the nodejs we can learn after some time
+
+console.log(3 
+    +
+     3) // code readability should be low
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique (used in React,Figma)
+// object
+
+/*to check variable data type use typeof function*/
+
+
+/*Interview Question*/
+console.log(typeof undefined); //output: undefined
+console.log(typeof null); //output: object
