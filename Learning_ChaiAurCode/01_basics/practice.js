4b06679c5a2f4c9a2d8403af93a813505cd34139
notalihassan <alihassan92199@gmail.com>
2025-06-21 15:17:42 +0500

    add

--- Learning_ChaiAurCode/01_basics/practice.js
+++ Learning_ChaiAurCode/01_basics/practice.js
@@ -1,8 +1,16 @@
-let x = 3 
-let y = x++
+let name = 'Ali Hassan'
+let age = 18
 
-console.log(x);
-console.log(y);
+console.log(`My name is ${name} and my age is ${age}`);
+
+const gameName = new String('Marvels')
+
+console.log(gameName.__proto__)
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
 
 
-console.log(x);
+
+
