0aac32b7a511174eea009fe86d8ade1bd20230c6
notalihassan <alihassan92199@gmail.com>
2025-06-21 11:11:24 +0500

    add

---
+++ Learning_ChaiAurCode/01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  //false
+console.log(null == 0);   //false
+console.log(null >= 0);   //true
+
+/* ans of all undefined is false */
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+/*it checks the equality and also data type*/
+console.log("2" === 2);
