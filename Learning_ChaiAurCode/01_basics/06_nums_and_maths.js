d269aea9bf5173d39a8ab05a4cd4ac71d7441cd1
notalihassan <alihassan92199@gmail.com>
2025-06-22 10:43:02 +0500

    add

--- Learning_ChaiAurCode/01_basics/06_nums_and_maths.js
+++ Learning_ChaiAurCode/01_basics/06_nums_and_maths.js
@@ -17,12 +17,12 @@ const hundreds = 1000000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
+// console.log(Math.abs(-4));  //output: gives only +ve value 4
+// console.log(Math.round(4.6));   //output: 5
+// console.log(Math.ceil(4.2));    //output: 5
+// console.log(Math.floor(4.9));   //output: 4
+// console.log(Math.min(4, 3, 6, 8));    //output: 3
+// console.log(Math.max(4, 3, 6, 8));    //output: 8
 
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
