137f0e37e0f2d25487a08ee37f140d751c0a4493
notalihassan <alihassan92199@gmail.com>
2025-06-24 09:02:58 +0500

    add

--- Learning_ChaiAurCode/01_basics/06_nums_and_maths.js
+++ Learning_ChaiAurCode/01_basics/06_nums_and_maths.js
@@ -21,14 +21,21 @@ const hundreds = 1000000
 // console.log(Math.round(4.6));   //output: 5
 // console.log(Math.ceil(4.2));    //output: 5
 // console.log(Math.floor(4.9));   //output: 4
-// console.log(Math.min(4, 3, 6, 8));    //output: 3
+// console.log(Math.min(4, 3, 6, 8));    //output: 3 
 // console.log(Math.max(4, 3, 6, 8));    //output: 8
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math);
+console.log(Math.random());   //random() genrate random number btw 0-1
+console.log(Math.random()*10);   //to get large number
+console.log((Math.random()*10) + 1);    //to get at least 1 bcz random() may generates 0. something 
+// that will become 0 output so thats why we add 1 
 
+/*It gives output btw 1-9 */
+console.log(Math.floor(Math.random()*10) + 1);  //gives random number (a single digit)
+
+
+/* Get at least min value which we defined and also get some increament using random() */
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
