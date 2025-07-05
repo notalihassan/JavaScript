8febf7a383b9a14ecc4b385a4d15b0441375a8ae
notalihassan <alihassan92199@gmail.com>
2025-07-05 19:21:48 +0500

    add

--- Learning_ChaiAurCode/03_basics/practice.js
+++ Learning_ChaiAurCode/03_basics/practice.js
@@ -1,82 +1,24 @@
-/*Functions*/
+/*
+Scope: local and global
 
-function myName() {
-    console.log("Ali Hassan");
-}
+local(block scope): if(){ let a=1 }
+global: let a = 1
 
-myName     //reference
-myName()   //execution
-
-
-function addTwoNumb(num1, num2) {
-    console.log(num1 + num2);
-}
-
-// addTwoNumb(3,7)   //output: 10
-// addTwoNumb(3,"h")   //output: 3h
-// addTwoNumb(3, null)   //output: 3
-
-
-//we can also check whether the given number is a number or not by if-else
-
-/*   not correct
-function addThreeNum(num1, num2, num3) {
-    if(num1 == Number && num2 == Number && num3 == Number){
-
-        console.log(num1 + num2 + num3);
-    }
-    else{
-        console.log("NaN");
-    }
-}
-
-addThreeNum(2,3,9);
+scope is {}
+scope if yh loops me hota ha 
+Scope ka matlb ha k code sirf utna hi execute hoga jitna uska scope hoga.
 */
 
-function addNum(num1,num2) {
-    console.log(num1 + num2);
+if(true){
+    let a = 1;
+    const b = 2;
+    var c = 3;
 }
 
-const result = addNum(2,5);   //output: 7
-console.log("Result = ", result);    //outpur: undefined
+//when we call a,b,c so only c will print due to var. so thats why we avoid var
 
-//to solve this problem we can do like this
-
-function subNum(num1, num2) {
-    let Result = num1 - num2;
-    // return Result;
-    //OR
-    return num1 - num2;
-}
-
-//now it gives the actual result
-console.log("Result = ", subNum(9,2));    //outpur: 9-2 = 7
+// console.log(a);   //error
+// console.log(b);   //error
+console.log(c);   // 3
 
 
-/*UserloginSystem*/
-
-function userLoggedIn(username) {
-
-    if(username === undefined){               //OR if(!username){}  same meanings
-        console.log("Please enter username!");
-        return
-    }
-    else{
-        return `${username} just logged in`;
-    }
-    
-}
-
-console.log(userLoggedIn());   //undefined bcz humne koi bhi argument pass nhi kia
-console.log(userLoggedIn("Ali_Hassan"));  
-
-
-
-function calculateCartPrice(num){
-    return num;
-}
-
-console.log(calculateCartPrice(12));  //output: 12
-console.log(calculateCartPrice(12,54,23)); //output: 12 bcz we give only one parameter in the function
-
-console.log("hello");
---
+++ Learning_ChaiAurCode/03_basics/practice1.js
@@ -0,0 +1,82 @@
+/*Functions*/
+
+function myName() {
+    console.log("Ali Hassan");
+}
+
+myName     //reference
+myName()   //execution
+
+
+function addTwoNumb(num1, num2) {
+    console.log(num1 + num2);
+}
+
+// addTwoNumb(3,7)   //output: 10
+// addTwoNumb(3,"h")   //output: 3h
+// addTwoNumb(3, null)   //output: 3
+
+
+//we can also check whether the given number is a number or not by if-else
+
+/*   not correct
+function addThreeNum(num1, num2, num3) {
+    if(num1 == Number && num2 == Number && num3 == Number){
+
+        console.log(num1 + num2 + num3);
+    }
+    else{
+        console.log("NaN");
+    }
+}
+
+addThreeNum(2,3,9);
+*/
+
+function addNum(num1,num2) {
+    console.log(num1 + num2);
+}
+
+const result = addNum(2,5);   //output: 7
+console.log("Result = ", result);    //outpur: undefined
+
+//to solve this problem we can do like this
+
+function subNum(num1, num2) {
+    let Result = num1 - num2;
+    // return Result;
+    //OR
+    return num1 - num2;
+}
+
+//now it gives the actual result
+console.log("Result = ", subNum(9,2));    //outpur: 9-2 = 7
+
+
+/*UserloginSystem*/
+
+function userLoggedIn(username) {
+
+    if(username === undefined){               //OR if(!username){}  same meanings
+        console.log("Please enter username!");
+        return
+    }
+    else{
+        return `${username} just logged in`;
+    }
+    
+}
+
+console.log(userLoggedIn());   //undefined bcz humne koi bhi argument pass nhi kia
+console.log(userLoggedIn("Ali_Hassan"));  
+
+
+
+function calculateCartPrice(num){
+    return num;
+}
+
+console.log(calculateCartPrice(12));  //output: 12
+console.log(calculateCartPrice(12,54,23)); //output: 12 bcz we give only one parameter in the function
+
+console.log("hello");
