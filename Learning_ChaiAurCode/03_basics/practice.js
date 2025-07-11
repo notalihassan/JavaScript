1a5eb30c1d4e5f93924688d9fce155a93f615f61
notalihassan <alihassan92199@gmail.com>
2025-07-11 19:10:29 +0500

    add

--- Learning_ChaiAurCode/03_basics/practice.js
+++ Learning_ChaiAurCode/03_basics/practice.js
@@ -23,10 +23,50 @@ user.welcomeMessage()
 console.log(this);   //output: {} empty object.
 /* it is due to js engine. In browser it gives the window object */
 
+
 function chai(){
     const username = "Ali";    
     console.log(this);
-    console.log(this.username);   //output: undefined ( we cannot acccess this in functions like this )
+    console.log(this.username);   //output: undefined ( we cannot acccess this in functions )
 }
 chai();
 
+//OR 
+
+// const CHAI = function code() {
+//     let username = "AlI HAssan"; 
+//     console.log(this);
+//     console.log(this.username);
+// }
+// CHAI();
+
+const CHAI = () => {
+    let username = "AlI HAssan"; 
+    console.log(this);
+    console.log(this.username);
+}
+CHAI();
+
+
+
+/*===================== Pure Arrow Function ==================*/
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+console.log(add(3,7));
+
+// OR ( implicit return )
+
+const addThree = (num1, num2, num3) => num1+num2+num3; 
+console.log(addThree(3,6,2));
+
+// OR
+
+const sub = (num1, num2) => (num1 - num2); 
+/* this method dont require any return statement and it is used in reactJS too much */
+console.log(sub(3,2));  
+
+// to return object using implicit return
+const SUB = (num1, num2) => ( {userName: "ali hassan"} );
+console.log(SUB(3,2));
