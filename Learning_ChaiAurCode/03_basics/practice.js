e93440dfd765cfd3a65fd6f5b8305dabfde62960
notalihassan <alihassan92199@gmail.com>
2025-07-11 18:48:39 +0500

    add

--- Learning_ChaiAurCode/03_basics/practice.js
+++ Learning_ChaiAurCode/03_basics/practice.js
@@ -3,8 +3,30 @@ const user = {
     price: 999,
 
     welcomeMessage: function(){
-        console.log(`${this.userName}, welcome to website.`);  /* this is the current context */
+        console.log(`${this.userName}, welcome to website.`);  
+        console.log(this);
+        
     }
 
 }
-/* Arrow k under this nhi hota but kio nhi hota yh hum jane gein*/
+/*
+ this is the current context 
+ current context: is te simple current value || kis ke bare me baat ho rhi ha 
+ Arrow k under this nhi hota but kio nhi hota yh hum jane gein 
+*/
+
+user.welcomeMessage()    // output: Ali Hassan, welcome to website.
+user.userName = "Serfo"  // now the userName will be changed to Serfo
+user.welcomeMessage()
+
+/* when we console this outside the scope */
+console.log(this);   //output: {} empty object.
+/* it is due to js engine. In browser it gives the window object */
+
+function chai(){
+    const username = "Ali";    
+    console.log(this);
+    console.log(this.username);   //output: undefined ( we cannot acccess this in functions like this )
+}
+chai();
+
