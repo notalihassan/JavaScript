550506ca9465e9c84011d04c7127d39751322aaa
notalihassan <alihassan92199@gmail.com>
2025-07-18 18:15:21 +0500

    add

---
+++ Learning_ChaiAurCode/04_control_flow/one.js
@@ -0,0 +1,14 @@
+//Conditional Statement
+
+if(true){
+    console.log("executed 1");
+}
+if( 2 == 2 ){
+    console.log("executed 2");
+}
+if( 3 != 2 ){
+    console.log("executed 3");
+}
+if( 2 === 2 ){
+    console.log("executed 4");
+}
