db0721535e1775bd5f4489d2228059d0f0312c09
notalihassan <alihassan92199@gmail.com>
2025-07-19 08:56:06 +0500

    add

---
+++ Learning_ChaiAurCode/04_control_flow/practice.js
@@ -0,0 +1,11 @@
+//Truthy and Falsy values
+
+// const userEmail = "ali@gmail.com"  //output: Got user email
+// const userEmail = ""               //output: not get user email
+// const userEmail = []               //output: Got user email
+
+if(userEmail){
+    console.log("Got user email")
+}else{
+    console.log("not get user email");
+}
---
+++ Learning_ChaiAurCode/04_control_flow/switch.js
@@ -0,0 +1,49 @@
+/* switch case */
+// //syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+/* 
+Window Shortcut Key:
+------------------
+alt + shift + down arrow (to get duplicate)
+*/
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("MOnth is Jan");
+        break;
+    case 2:
+        console.log("MOnth is Feb");
+        break;
+    case 3:
+        console.log("MOnth is Mar");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
+
+const Month = "Feb"
+switch (Month) {
+    case "Jan":
+        console.log("MOnth is Jan");
+        break;
+    case "Feb":
+        console.log("MOnth is Feb");
+        break;
+    case "Mar":
+        console.log("MOnth is Mar");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
