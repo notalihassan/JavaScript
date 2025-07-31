8eba6eccec616f85fc8f5f3e1fcff4e3361c5cdd
notalihassan <alihassan92199@gmail.com>
2025-07-31 23:05:21 +0500

    add

---
+++ Learning_ChaiAurCode/07_projects/02_bmiCalculator/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form')
+
+
+form.addEventListener('submit', (evnt) => {
+    evnt.preventDefault();   //it prevents default action for minute
+
+    const height = parseInt(document.querySelector('#height').value) 
+    /* .value gives the value and parseInt is due to give only integer value */
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if( height == '' || height < 0 || isNaN(height)){ 
+        //insted of doing "height != NaN" use isNaN() function 
+        alert("Please give a valid Height!");
+    }else if( weight == '' || weight < 0 || isNaN(weight) ){
+        alert("Please give a valid Weight!");
+    }else{
+        const bmi = (weight / ((height*height) / 10000)).toFixed(2);  //.toFixed() is used get only specific number
+        //show results 
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+    
+
+    //show weight guide
+
+    
+
+
+
+})
