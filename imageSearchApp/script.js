12b92eb9e70c2ff12e4c6b371641d6c28cbb1538
notalihassan <alihassan92199@gmail.com>
2025-08-24 23:02:19 +0500

    new project

---
+++ imageSearchApp/script.js
@@ -0,0 +1,3 @@
+const accessKey = 'sjKwO9k-puOwM3onQxn0azn6lVyHKkmT7dhSinM2Fvw'
+const searchForm = document.querySelector('form')
+const imageContainter = document.querySelector('.image-container')
